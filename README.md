017fe4ae920faa6052c63c1274ba946e3b020c26
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-21 11:10:20 -0400

    Basic page layout

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Template
-This is a template to use for new repos
-Has bundling setup and css from webpack.
-Webpack commands
-- npm run build
-- npm run watchnpm init @eslint/config@latest
-- npm run lint:fix
+# Weather App
+
+This is the weater project from TOP
+
+It is meant to teach how to use fetch and use an API as long with await/async
+
+This is also my first attempt at using BEM. It is rather confusing at first but I am slowly getting the hang of it
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
-import { test } from './test.js';
+// const key = 'd317c44b140242988c7140818242005';
 
-console.log('this is from main');
+// const urlTest = `http://api.weatherapi.com/v1/current.json?key=${key}&q=London`;
 
-test();
+// // async function testAPI() {
+// //   const response = await fetch(
+// //     `http://api.weatherapi.com/v1/current.json?key=${key}&q=30041`,
+// //     { mode: 'cors' },
+// //   );
+// //   const weatherData = await response.json();
+// //   console.log(weatherData);
+// //   console.log(`${weatherData.location.name} ${weatherData.location.country}`);
+// //   console.log(weatherData.location.localtime);
+// //   console.log(weatherData.current.condition.text);
+// //   console.log(weatherData.current.condition.icon);
+// //   console.log(weatherData.current.feelslike_f);
+// // }
 
-console.log('test');
+// async function testforecastAPI() {
+//   const response = await fetch(
+//     `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=30041&days=3`,
+//     { mode: 'cors' },
+//   );
+//   const weatherData = await response.json();
+//   console.log(weatherData);
+// }
+
+// testforecastAPI();
+
+// console.log(document.querySelector('.forecast__dayOne'));

8cec14f46958694e1ec252e8ef5031727fdc1f76
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-22 10:49:47 -0400

    Some layout responiveness added
    Moved main logic to change webpage to main.js

--- src/scripts/currentWeatherAPI.js
+++ src/scripts/currentWeatherAPI.js
@@ -14,32 +14,30 @@ import {
   changeRainChance,
 } from './changeDOM';
 
-searchButton.addEventListener('click', () => {
-  currentWeather();
-  changeForecast();
-  searchValue.value = '';
-});
-
-async function currentWeather() {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${key}&q=${searchValue.value}`,
-    { mode: 'cors' },
-  );
-  const currentData = await response.json();
-  //   console.log(currentData);
-  changeCurrentCondition(currentData.current.condition.text);
-  changeLocation(
-    currentData.location.name,
-    currentData.location.region,
-    currentData.location.country,
-    currentData.location.localtime,
-  );
-  changeCurrentImage(currentData.current.condition.icon);
-  changeCurrentTemp(currentData.current.feelslike_f);
-  changeCurrentHumidity(currentData.current.humidity);
-  changeCurrentUV(currentData.current.uv);
-  changeWindSpeed(currentData.current.gust_mph);
-  changeWindDirection(currentData.current.wind_dir);
-  changeCloudCoverage(currentData.current.cloud);
-  //   changeRainChance(currentData.current.)
+export async function currentWeather() {
+  document.documentElement.style.setProperty('--visible-state', 'hidden');
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${key}&q=${searchValue.value}`,
+      { mode: 'cors' },
+    );
+    const currentData = await response.json();
+    changeCurrentCondition(currentData.current.condition.text);
+    changeLocation(
+      currentData.location.name,
+      currentData.location.region,
+      currentData.location.country,
+      currentData.location.localtime,
+    );
+    changeCurrentImage(currentData.current.condition.icon);
+    changeCurrentTemp(currentData.current.feelslike_f);
+    changeCurrentHumidity(currentData.current.humidity);
+    changeCurrentUV(currentData.current.uv);
+    changeWindSpeed(currentData.current.gust_mph);
+    changeWindDirection(currentData.current.wind_dir);
+    changeCloudCoverage(currentData.current.cloud);
+    document.documentElement.style.setProperty('--visible-state', 'visible');
+  } catch (error) {
+    alert('Location Not Found');
+  }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,6 +27,33 @@
 
 // console.log(document.querySelector('.forecast__dayOne'));
 
-import { test } from './currentWeatherAPI';
+import { currentWeather } from './currentWeatherAPI';
+import { searchValue, searchButton } from './domElements';
+import { changeForecast } from './changeForecast';
 
 export const key = 'd317c44b140242988c7140818242005';
+const horizontalScroll = document.querySelector('.flex-grid__additional');
+horizontalScroll.addEventListener('wheel', (event) => {
+  event.preventDefault();
+
+  horizontalScroll.scrollBy({
+    left: event.deltaY < 0 ? -30 : 30,
+  });
+});
+searchButton.addEventListener('click', () => {
+  currentWeather();
+  changeForecast();
+  searchValue.value = '';
+});
+// change on enter press when input field is focused
+document.querySelector('.input-field').addEventListener('focus', () => {
+  document
+    .querySelector('.input-field')
+    .addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        currentWeather();
+        changeForecast();
+        searchValue.value = '';
+      }
+    });
+});
